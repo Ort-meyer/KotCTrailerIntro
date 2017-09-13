157700cb484b3dd315def0615d6e36fe5db83150
Fetdressing <simon.home@hotmail.com>
2017-09-13 21:53:33 +0200

    changed alot

---
+++ Assets/Scripts/AnimationSpeedModifier.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationSpeedModifier : MonoBehaviour {
+    public float speedIncrease = 1.5f;
+    public bool thisOnly = false;
+    // Use this for initialization
+    void Start()
+    {
+        foreach (AnimationState state in GetComponent<Animation>())
+        {
+            state.speed = speedIncrease;
+            if(thisOnly)
+            {
+                break;
+            }
+        }
+
+    }
+
+    void Awake()
+    {
+        foreach (AnimationState state in GetComponent<Animation>())
+        {
+            state.speed = speedIncrease;
+            if (thisOnly)
+            {
+                break;
+            }
+        }
+
+    }
+
+    public void ChangeSpeed(float newspeed)
+    {
+        foreach (AnimationState state in GetComponent<Animation>())
+        {
+            state.speed = newspeed;
+            if (thisOnly)
+            {
+                break;
+            }
+        }
+    }
+
+}

fe6652cd72d3841fa2ceb86512a8521fae85c9b6
Jawsarn <christian_markowicz@hotmail.com>
2017-09-14 17:01:49 +0200

    Push

--- Assets/SwirldFoward.cs
+++ Assets/SwirldFoward.cs
@@ -8,16 +8,21 @@ public class SwirldFoward : MonoBehaviour {
     Vector3 startPos;
 
     float rotated = 0;
-    float rotateAmount = 5.0f;
+
 
 
     bool started = false;
-    float timeToMove = 1.0f;
+    static float laps = 1.0f;
+    static float timeToMove = 0.5f;
+    static float rotateAmount = laps*6.0f*1.0f/timeToMove;
     float curTime = 0.0f;
     Animation anim;
+
+    bool hasDoneStuff = false;
 	// Use this for initialization
 	void Start () {
         anim = GameObject.Find("TrailerSceneIntroPushbutton").GetComponent<Animation>();
+        anim["TrailerSceneIntro_Pushbutton"].speed = 1.5f;
         startPos = transform.position;
     }
 	
@@ -40,20 +45,21 @@ public class SwirldFoward : MonoBehaviour {
 
         curTime += Time.deltaTime;
 
-
-        if (clipTime > 0.5f && anim.isPlaying)
-        {
-            anim.enabled = false;
-        }
-        if (curTime > 0.8f && anim.isPlaying == false)
-        {
-            anim.enabled = true;
-        }
-
         float alpha = Mathf.Min(curTime / timeToMove, 1.0f);
+        if (clipTime > 0.2f && anim.isPlaying && hasDoneStuff == false)
+        {
+            hasDoneStuff = true;
+            anim["TrailerSceneIntro_Pushbutton"].speed = 0;
+        }
+        if (alpha > 0.8f)
+        {
+            anim["TrailerSceneIntro_Pushbutton"].speed = 1.5f;
+        }
+
+
 
         transform.position = startPos * (1.0f - alpha) + endpos.position * alpha;
-        if (rotated < 360)
+        if (rotated < 360* laps)
         {
             transform.Rotate(new Vector3(0, 0, rotateAmount));
             rotated += rotateAmount;

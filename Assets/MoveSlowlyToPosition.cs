94427fd15b446fd191e322afb27f0d4eae73d5f5
Fetdressing <simon.home@hotmail.com>
2017-09-15 11:42:40 +0200

    meck

--- Assets/MoveSlowlyToPosition.cs
+++ Assets/MoveSlowlyToPosition.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class MoveSlowlyToPosition : MonoBehaviour
 {
+    bool hasStarted = false;
 
     public Transform m_targetPosition;
     public float m_moveSpeed;
 
-
+    Quaternion startRot;
+    public float rotTime = 2.0f;
+    float startTime = 0.00000000000000f;
 
     // Use this for initialization
     void Start()
     {
-        
+        startRot = transform.rotation;
     }
 
     // Update is called once per frame
@@ -22,6 +25,10 @@ public class MoveSlowlyToPosition : MonoBehaviour
         if (GameObject.Find("CameraSwitcher").GetComponent<CameraSwitcher>().started == false)
         {
             return;
+        }
+        else if(startTime < 0.0001)
+        {
+            startTime = Time.time;
         }
 
         if ((m_targetPosition.position - transform.position).magnitude < 0.01f)
@@ -32,5 +39,9 @@ public class MoveSlowlyToPosition : MonoBehaviour
 
         Vector3 moveDirection = -1 * (transform.position - m_targetPosition.position).normalized;
         transform.position = transform.position + moveDirection * m_moveSpeed * Time.deltaTime;
+
+        transform.rotation = Quaternion.Slerp(startRot, m_targetPosition.rotation, Mathf.Min(((Time.time - startTime) / rotTime), 1.0f));
+
+        print(Mathf.Min(((Time.time - startTime) / rotTime), 1.0f).ToString());
     }
 }
